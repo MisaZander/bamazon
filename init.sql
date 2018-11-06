1d9e764cbbaa2ac29b6cf1f0f5f1792e8b9538c0
Alexander Osborn <misterzander@gmail.com>
2018-11-06 14:39:50 -0600

    Break up the sql file

--- init.sql
+++ init.sql
@@ -11,18 +11,3 @@ CREATE TABLE products(
     price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT(11) NOT NULL DEFAULT 0
 );
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES
-('Sack O Chocolate Diddlers', 'Confections', 10.99, 15),
-('Neopets Trading Card', 'Games', 0.99, 999),
-('Can\'t Catch Harry Card Game', 'Games', 29.99, 50),
-('rogersimon10 Brand Jumper Cables', 'Automotive', 45.98, 1),
-('5 Pound Gummy Worm', 'Confections', 24.95, 30),
-('Lavender Bath Salts', 'Bathroom', 11.99, 40),
-('Toilet Bidet', 'Bathroom', 25.00, 5),
-('64-Pack Toilet Paper', 'Bathroom', 42.88, 25),
-('\"Twilight\" Book on Tape', 'Books', 19.99, 8492),
-('Neck Basket', 'Utilities', 19.99, 62);
-
-SELECT * FROM products;
