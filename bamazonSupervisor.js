8287aa7cb4ca54303847fc9a536ea4480b6b8605
Alexander Osborn <misterzander@gmail.com>
2018-11-08 15:26:25 -0600

    Successful test of the ultimate query

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -14,3 +14,44 @@ var connection = sequel.createConnection({
     password: process.env.DATABASE_PASS
 });
 
+inquire.prompt([
+    {
+        name: "query",
+        type: "list",
+        message: "Hello person of ultimate superiority. What would you like to do?",
+        choices: ["View Product Sales by Department", "Create New Department"]
+    }
+]).then(function(response) {
+    switch(response.query) {
+        case "View Product Sales by Department":
+            viewSales();
+        break;
+        case "Create New Department":
+            newDepartment();
+        break;
+        default: 
+            console.log("Inquirer has failed. Please power off your machine and await the singularity");
+        break;
+    }
+}); //inquire then
+
+function viewSales() {
+    var theUltimateQuery = "SELECT " + 
+                                "d.department_id AS 'Department ID', " +
+                                "d.department_name AS 'Department Name', " +
+                                "d.over_head_costs AS 'Overhead Costs', " + 
+                                "SUM(p.product_sales) AS 'Total Sales' " +
+                            "FROM " +
+                                "departments AS d " +
+                            "LEFT JOIN " +
+                                "products AS p " + 
+                            "ON " + 
+                                "d.department_name = p.department_name " +
+                            "GROUP BY " +
+                                "d.department_id";
+    connection.query(theUltimateQuery, function(queryErr, queryRes) {
+        if(queryErr) throw queryErr;
+        console.log(queryRes);
+        connection.end();
+    });//Mega select query
+}//viewSales()
